67a434bf6b0e9a9d49aeee2ed36f150ce6d36823
Zakaria <zakjama0@gmail.com>
2024-03-18 16:09:39 +0000

    Finished Lab

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -15,6 +15,7 @@ date.addEventListener("click", (e) =>{
 const form = document.querySelector("#todo-form");
 const list = document.querySelector("#list");
 const input = document.querySelector("#new-todo");
+const completedList = document.querySelector("#completedList");
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
@@ -27,12 +28,37 @@ form.addEventListener("submit", (e) =>{
     
     // add delete button to task
     const btn = document.createElement("button");
-    btn.innerText = "delete"
-    list.appendChild(btn);
+    btn.innerHTML = "\u{1F5D1}";
+    btn.style.border = "none";
+    btn.style.cursor = "pointer";
+    newListItem.appendChild(btn);
 
+    // each button has a listener for deleting the entry
     btn.addEventListener("click", (e) =>{
         newListItem.remove();
-        btn.remove();
     })
+    btn.addEventListener("mouseover", (e) =>{
+        btn.style.fontSize = "20px"
+    })
+    btn.addEventListener("mouseleave", (e) =>{
+        btn.style.fontSize = "10px"
+    })
+
+    // add a checkbox for completing tasks
+    const check_input = document.createElement("input")
+    check_input.type = "checkbox";
+    newListItem.append(check_input);
+
+    // Change task to green if completed
+    check_input.addEventListener("change", (e) => {
+        if (check_input.checked){
+            newListItem.style.color = "green";
+            completedList.appendChild(newListItem)
+        } else {
+            newListItem.style.color = "black";
+            list.appendChild(newListItem);
+        }    
+    });
+
 });
 
